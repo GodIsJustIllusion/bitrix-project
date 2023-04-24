1c5a276b9e5ef89ab5f4a7cb4ef4d7bad84d1b6e
Igor_A <87186964+GodIsJustIllusion@users.noreply.github.com>
2023-04-24 17:21:45 +0400

    Delete bitrix/cache directory

--- bitrix/cache/default/72/725f13d52cec0e7966f4ede6d88ebbef.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001681474933';
-$dateexpire = '001681478533';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:0:{}}';
-return true;
-?>
